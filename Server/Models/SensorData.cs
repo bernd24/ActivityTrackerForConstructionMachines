bc1da6e755f6b8b8dc1e7b2f70112816b573951e
Bernd Diekmann <bernd.diekmann@hs-osnabrueck.de>
2022-10-05 14:46:54 +0200

    adjusted JSON Format

--- Server/Models/SensorData.cs
+++ Server/Models/SensorData.cs
@@ -6,9 +6,6 @@ namespace Server.Models{
 
     public class SensorData<T>{
         public int node_ID {get; set;}
-        public int type {get; set;}
         public List<T> payload {get; set;}
-
-        public int packets {get; set;}
     }
 }
