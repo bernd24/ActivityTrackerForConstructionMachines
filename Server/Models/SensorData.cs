c206c63fc4fd776159fa42b4ef1795094ba35000
Bernd Diekmann <bernd.diekmann@hs-osnabrueck.de>
2022-10-05 11:12:58 +0200

    added ESP Interface

---
+++ Server/Models/SensorData.cs
@@ -0,0 +1,12 @@
+namespace Server.Models{
+    public class Handshake{
+        public string ID {get; set;}
+        public int elements {get; set;}
+    }
+
+    public class SensorData<T>{
+        public int node_ID {get; set;}
+        public int type {get; set;}
+        public List<T> payload {get; set;}
+    }
+}
