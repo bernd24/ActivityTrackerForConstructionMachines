ad33480fbde3a8f3d1a1282839dd9351f7c47e03
Leander Nordmann <leander.nordmann@hs-osnabrueck.de>
2022-10-07 17:55:44 +0200

    add differnt charts to compare them

---
+++ Server/Controllers/ChartController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+using Microsoft.AspNetCore.Authorization;
+using SqlKata;
+using SqlKata.Execution;
+using System.Data.SqlClient;
+
+namespace Server.Controllers;
+
+[Authorize]
+public class ChartController : Controller
+{
+    private readonly ILogger<ChartController> _logger;
+    private readonly QueryFactory Db;
+
+    public ChartController(ILogger<ChartController> logger, QueryFactory db)
+    {
+        _logger = logger;
+        Db = db;
+    }
+    [AllowAnonymous]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
