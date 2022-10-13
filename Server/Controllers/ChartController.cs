fd43c02431fc7117934a0b9e96d0a429a521e64f
Bernd Diekmann <bernd.diekmann@hs-osnabrueck.de>
2022-10-13 14:24:55 +0200

    connected database with chart

--- Server/Controllers/ChartController.cs
+++ Server/Controllers/ChartController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Authorization;
 using SqlKata;
 using SqlKata.Execution;
 using System.Data.SqlClient;
+using System.Globalization;
+
 
 namespace Server.Controllers;
 
@@ -20,9 +22,42 @@ public class ChartController : Controller
         Db = db;
     }
     [AllowAnonymous]
-    public IActionResult Index()
+    public IActionResult Index(int Id)
     {
-        return View();
+        System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+        customCulture.NumberFormat.NumberDecimalSeparator = ".";
+
+        System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+        var sensors = Db.Query("Measurement")
+        .Select("Sensor.SensorName","Sensor.SensorType","SensorInstance.Axis","Measurement.SI_Id")
+        .Where("WS_Id",Id)
+        .GroupBy("SI_Id")
+        .Join("SensorInstance","SensorInstance.Id","Measurement.SI_Id")
+        .Join("Sensor","Sensor.Id","SensorInstance.S_Id")
+        .Get();
+
+        var ret = new List<MeasureData>();
+
+        foreach(var s in sensors){
+            var result = Db.Query("Measurement")
+            .Where("WS_Id",Id)
+            .Where("SI_Id",(int)s.SI_Id)
+            .Get<Measurement>();
+            List<float> datapoints = new List<float>();
+            List<String> timestamps = new List<String>();
+            int i = 0;
+            foreach(Measurement m in result){
+                datapoints.Add(m.SensorData);
+                timestamps.Add(m.TimeOfMeasure.AddMilliseconds(i*100).ToString());
+                i++;
+            }
+            ret.Add(new MeasureData{
+                sensorName = s.SensorName + " " + s.SensorType + (s.Axis == null ? "" : " " + s.Axis + "-Axis"),
+                datapoints = datapoints,
+                timestamps = timestamps
+            });
+        }
+        return View(ret);
     }
 
 
--- Server/wwwroot/js/lineChart.js
+++ Server/wwwroot/js/lineChart.js
@@ -1,43 +1,4 @@
 
-    const labels = [0,1,2,3,4,5,6,7,8,9,10,11,12];
 
-    const datapoints = [0, 20, 20, 60, 60, 120, NaN, 180, 120, 125, 105, 110, 170];
-    const data = {
-    labels: labels,
-    datasets: [
-        {
-        label: 'Cubic interpolation (monotone)',
-        data: datapoints,
-        borderColor: 'rgb(100, 99, 132)',
-        //  @* borderColor: Utils.CHART_COLORS.blue,     this shit is not working  Utils file is not there https://github.com/chartjs/Chart.js/blob/master/docs/scripts/utils.js*@  
-        //  @* but i dont think we need that *@
-        fill: false,
-        cubicInterpolationMode: 'monotone',
-        tension: 0.4
-        }, {
-        label: 'Cubic interpolation',
-        data: datapoints,
-        borderColor: 'rgb(255, 99, 132)',
-        fill: false,
-        tension: 0.4
-        }, {
-        label: 'Linear interpolation (default)',
-        data: datapoints,
-        borderColor: 'rgb(200, 99, 50)',
-        fill: false
-        }
-    ]
-    };
-
-  const config = {
-    type: 'line',
-    data: data,
-    options: {}
-  };
-
-    const myChart = new Chart(
-    document.getElementById('myLineChart'),
-    config 
-  );
 
 
