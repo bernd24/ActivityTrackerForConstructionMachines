19276d8d8eba1192bcb94807ef0e9b890ba66845
Bernd Diekmann <bernd.diekmann@hs-osnabrueck.de>
2022-10-19 10:38:56 +0200

    activitytobinary prepare merge

---
+++ Server/activitytobinary/Data.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+struct Data {
+    std::string filePath;
+    std::string sensorName;
+    std::string sensorType;
+    std::string sensorAxis;
+    std::string si_id;
+};
