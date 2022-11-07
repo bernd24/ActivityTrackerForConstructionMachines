a69c034b71432e315db62de3730027a38dc54c36
Bernd Diekmann <bernd.diekmann@hs-osnabrueck.de>
2022-11-07 17:34:45 +0100

    demo

--- Server/Controllers/ChartController.cs
+++ Server/Controllers/ChartController.cs
@@ -106,6 +106,90 @@ public class ChartController : Controller
         return View(ret);
     }
 
+    [HttpGet("Chart/Index/{Id}/IndexJson")]
+    public IActionResult IndexJson(int Id, DateTime fromDate, DateTime fromTime, DateTime tillDate, DateTime tillTime)
+    {
+        System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+        customCulture.NumberFormat.NumberDecimalSeparator = ".";
+
+        System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+        var sensors = Db.Query("Measurement")
+        .Select("Sensor.SensorName","Sensor.SensorType","SensorInstance.Axis","Measurement.SI_Id","SensorInstance.SN_Id")
+        //.Where("WS_Id",Id)
+        .GroupBy("SI_Id")
+        .Join("SensorInstance","SensorInstance.Id","Measurement.SI_Id")
+        .Join("Sensor","Sensor.Id","SensorInstance.S_Id")
+        .OrderBy("SensorInstance.SN_Id")
+        .Get();
+
+        DateTime from = new DateTime(fromDate.Year,fromDate.Month,fromDate.Day,fromTime.Hour,fromTime.Minute,fromTime.Second);
+        DateTime till = new DateTime(tillDate.Year,tillDate.Month,tillDate.Day,tillTime.Hour,tillTime.Minute,tillTime.Second);
+        DateTime lowestFrom = new DateTime();
+
+        var ret = new List<NodeData>();
+
+        foreach(var s in sensors){
+            SqlKata.Query query;
+            if(fromDate.Year == 1){
+                query = Db.Query("Measurement")
+                .Where("WS_Id",Id)
+                .Where("SI_Id",(int)s.SI_Id)
+                .OrderBy("TimeOfMeasure")
+                .OrderBy("Nr");
+            }
+            else{
+                query = Db.Query("Measurement")
+                .Where("WS_Id",Id)
+                .Where("SI_Id",(int)s.SI_Id)
+                .Where("TimeOfMeasure",">",from.AddSeconds(-10))
+                .Where("TimeOfMeasure","<",till)
+                .OrderBy("TimeOfMeasure")
+                .OrderBy("Nr");
+            }
+            var result = query.Get<Measurement>();
+
+            if(result.Count() > 0 && from.Year == 1 && (lowestFrom.Year == 1 || lowestFrom > result.First().TimeOfMeasure)){
+                from = result.First().TimeOfMeasure;
+            }
+
+            List<Point> datapoints = new List<Point>();
+            int i = 0;
+            foreach(Measurement m in result){
+                if(m.Nr == 0){
+                    i = 0;
+                }
+                DateTime NrTime = m.TimeOfMeasure.AddMilliseconds(i*100);
+                if(tillDate.Year != 1 && NrTime > till){
+                    break;
+                }
+                if(NrTime >= from){
+                    datapoints.Add(new Point{x = ((DateTimeOffset)NrTime).ToUnixTimeMilliseconds(), y = m.SensorData});
+                }
+                if(NrTime > till){
+                    till = NrTime;
+                }
+                i++;
+            }
+            if(ret.Count == 0 || ret.Last().SN_Id != s.SN_Id){
+                ret.Add(new NodeData{
+                    SN_Id = s.SN_Id,
+                    data = new List<MeasureData>()
+                });
+            }
+            ret.Last().data.Add(new MeasureData{
+                sensorName = s.SensorName + " " + s.SensorType + (s.Axis == null ? "" : " " + s.Axis + "-Axis"),
+                datapoints = datapoints
+            });
+        }
+        if(from.Year == 1){
+            from = lowestFrom;
+        }
+        ViewBag.fromDate = from;
+        ViewBag.fromTime = from;
+        ViewBag.tillDate = till;
+        ViewBag.tillTime = till;
+        return Json(ret);
+    }
 
     public IActionResult SpecificMaschine()
     {
